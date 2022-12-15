fa39e468c2edb999cfc60a59beec7b242bf95791
YYJ-1229 <90436@naver.com>
2022-12-15 15:44:15 +0900

    [feat] 일기 수정 기능 추가

--- main.js
+++ main.js
@@ -19,6 +19,14 @@ var app = http.createServer(function(request, response) {
         topic.create(request, response);
     } else if(pathname === '/create_process') {
         topic.create_process(request,response);
+    } else if(pathname === '/update') {
+        topic.update(request, response);
+    } else if(pathname === '/update_process') {
+        topic.update_process(request, response);
+    } else if(pathname === '/update') {
+      topic.update(request, response);
+    } else if(pathname === '/update_process') {
+      topic.update_process(request, response);
     } else {
         response.writeHead(404);
         response.end('Not found');
